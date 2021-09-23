bdf164227c6284ddf6d4960c9c889c50e029a4e4
majunon <fayardjul@gmail.com>
2021-09-23 22:56:16 +0200

    still working on special case

--- script.js
+++ script.js
@@ -1,94 +1,157 @@
+const maxValue = 10000000000;
+const minValue = -1000000000;
+
 // MATHEMATICS
 
 function add(a, b) {
-  let c=parseInt(a);
-  let d=parseInt(b);
-  return (c + d);
+  let c = parseInt(a);
+  let d = parseInt(b);
+  let res = c + d;
+
+  if(res < maxValue && res > minValue){
+    return (res);
+  }
+  else{
+    return ("overflow");
+  }
 };
 
 function substract(a, b) {
-  let c=parseInt(a);
-  let d=parseInt(b);
-  return (a - b);
+  let c = parseInt(a);
+  let d = parseInt(b);
+  let res = c - d;
+  if(res < maxValue && res > minValue){
+    return (res);
+  }
+  else{
+    return ("overflow");
+  }
 };
 
 function multiply(a, b) {
-  let c=parseInt(a);
-  let d=parseInt(b);
-  return (a * b);
+  let c = parseInt(a);
+  let d = parseInt(b);
+  let res = c * d;
+
+  if(res < maxValue && res > minValue){
+    return (res);
+  }
+  else{
+    return ("overflow");
+  }
 };
 
 function divide(a, b) {
-  let c=parseInt(a);
-  let d=parseInt(b);
-  return (a / b);
+  let c = parseInt(a);
+  let d = parseInt(b);
+  let res = c / d;
+
+  if(d == 0){
+    return ("infinity");
+  }
+  else if(res < maxValue && res > minValue){
+    return(res);
+  }
+  else{
+    return ("overflow");
+  }
 };
 
 // This is the function that do the calculation
 
 function operate(operator, a, b) {
   switch (operator) {
-    case "plus":
+    case '+':
       return (add(a, b));
       break;
-    case "minus":
+    case '-':
       return (substract(a, b));
       break;
-    case "multiply":
+    case '*':
       return (multiply(a, b));
       break;
-    case "divide":
+    case '/':
       return (divide(a, b));
       break;
   }
 };
 
-// These are the variables in which are stocked the values to display
-
-let displayFirstLine="", displaySecondLine="", displayThirdLine="";
-
 // These are the variables used to do the calculation
 
-let operatorSelection=0, firstOperand=0, secondOperand=0;
+let operator, firstOperand, secondOperand;
 
 // Refresh the display
-function display(){
+
+function display() {
   const thirdLine = document.querySelector('.thirdLine');
   const secondLine = document.querySelector('.secondLine');
   const firstLine = document.querySelector('.firstLine');
 
-  thirdLine.textContent=displayThirdLine;
-  secondLine.textContent=displaySecondLine;
-  firstLine.textContent=displayFirstLine;
+  thirdLine.textContent = firstOperand;
+  secondLine.textContent = operator;
+  firstLine.textContent = secondOperand;
 };
+display(); //init the display
+
+// When a number is clicked
 
 const numbers = document.querySelectorAll('.number');
 numbers.forEach(number => {
-  number.addEventListener('click',function(e){
-    displayThirdLine+=this.textContent;
+  number.addEventListener('click', function (e) {
+    if(!firstOperand){
+      firstOperand=this.textContent;
+    }
+    else{
+      firstOperand+=this.textContent;
+    }
     display();
   });
 });
 
+// When an operator is clicked
+
 const operators = document.querySelectorAll('.operator');
 operators.forEach(operator => {
-  operator.addEventListener('click',function(e){
-    operatorSelection=this.id;
-    displaySecondLine=displayThirdLine;
-    firstOperand=displayThirdLine;
-    displayThirdLine=this.textContent;
+  operator.addEventListener('click', function (e) {
+    operator = this.textContent;
+    secondOperand = 
     display();
-    displayFirstLine=displaySecondLine;
-    displaySecondLine=displayThirdLine;
-    displayThirdLine="";
   })
-})
+});
+
+// When = is clicked
 
 const enter = document.querySelector('#equal');
-enter.addEventListener('click',function(e){
-  displayFirstLine=displaySecondLine+displayThirdLine;
-  displaySecondLine="=";
-  secondOperand=displayThirdLine;
-  displayThirdLine=operate(operatorSelection, firstOperand, secondOperand);
+enter.addEventListener('click', function (e) {
+  if(operatorSelection == 0){
+    return;
+  }
+  secondOperand = displayThirdLine;
+  displayFirstLine = firstOperand+displaySecondLine+secondOperand;
+  displayThirdLine = operate(operatorSelection, firstOperand, secondOperand);
+  firstOperand = displayThirdLine;
+  displaySecondLine = "=";
+  operatorSelection=0;
+  display();
+})
+
+// When clear is clicked
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click',function(e){
+  displayFirstLine = "";
+  displaySecondLine = "";
+  displayThirdLine = "0";
+  operatorSelection = 0;
+  firstOperand = 0;
+  secondOperand = 0;
+  display();
+})
+
+// When delete is clicked
+
+const backspace = document.querySelector('#delete');
+backspace.addEventListener('click',function(e){
+  displayThirdLine=displayThirdLine.slice(0,-1);
   display();
 })
